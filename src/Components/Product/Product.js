c86345533cc6c156ee6b1a8e2936e7dc3c7a51ef
Phoenix Shane <phoenix.oconnell@gmail.com>
2020-01-21 12:30:13 -0600

    Began sass styling 0121201330

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -10,13 +10,13 @@ class Product extends Component {
     }
     render() {
         return (
-            <div>
-                <Link to='/dashboard'><button>Back</button></Link>
+            <div className='product-container'>
+                <Link to='/dashboard'><button className='back-button'>Back</button></Link>
                 <span>{this.props.product.product_name}</span>
-                <img src={this.props.product.product_img} alt='Product Image' />
-                <span>{this.props.product.product_price}</span>
+                <img src={this.props.product.product_img} alt='Product Image' style={{maxWidth: '500px'}}/>
+                <span><span className='normal-text'>$</span>{`${this.props.product.product_price}`}</span>
                 <span>{this.props.product.product_desc}</span>
-                <div>
+                <div className='product-buttons'>
                     {this.props.isAdmin ? <Link to={`/dashboard/edit/${this.props.product.product_id}`}><button>Edit</button></Link> : null}
                     <button>Purchase</button>
                     {this.props.isAdmin ? <Link to='/dashboard'><button onClick={() => this.props.deleteProduct(this.props.product.product_id)}>Delete</button></Link> : null}
